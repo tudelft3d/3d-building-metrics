ccbfef612f7a640effbbe8925b7b635ccaec6fe1
Stelios Vitalis <steliosvitalis@gmail.com>
2021-06-16 14:57:12 +0200

    Refactor things

--- volume.py
+++ volume.py
@@ -13,7 +13,8 @@ with open(filename, 'r') as f:
 if "transform" in cm:
     s = cm["transform"]["scale"]
     t = cm["transform"]["translate"]
-    verts = [[v[0] * s[0] + t[0], v[1] * s[1] + t[1], v[2] * s[2] + t[2]] for v in cm["vertices"]]
+    verts = [[v[0] * s[0] + t[0], v[1] * s[1] + t[1], v[2] * s[2] + t[2]]
+             for v in cm["vertices"]]
 else:
     verts = cm["vertices"]
 
@@ -24,14 +25,19 @@ print("id, actual volume, convex hull volume, area")
 for obj in cm["CityObjects"]:
     building = cm["CityObjects"][obj]
 
+    # TODO: Add options for all skip conditions below
+
+    # Skip if type is not Building or Building part
     if not building["type"] in ["Building", "BuildingPart"]:
         continue
 
+    # Skip if no geometry
     if not "geometry" in building or len(building["geometry"]) == 0:
         continue
 
     geom = building["geometry"][0]
 
+    # Skip if the geometry type is not supported
     if geom["type"] == "MultiSurface":
         boundaries = geom["boundaries"]
     elif geom["type"] == "Solid":
@@ -40,11 +46,13 @@ for obj in cm["CityObjects"]:
         continue
 
     f = [[len(r[0])] + r[0] for r in [f for f in boundaries]]
-    # mesh faces
-    faces = np.hstack(f)    # triangle
+    faces = np.hstack(f) 
 
+    # Create the pyvista object
     surf = pv.PolyData(vertices, faces)
 
-    ch_volume = ss.ConvexHull([verts[i] for i in np.array(geom["boundaries"]).flatten()]).volume
+    # Compute the convex hull volume
+    points = [verts[i] for i in np.array(geom["boundaries"]).flatten()]
+    ch_volume = ss.ConvexHull(points).volume
 
     print(f"{obj}, {building['type']}, {surf.volume}, {ch_volume}, {surf.area}")
