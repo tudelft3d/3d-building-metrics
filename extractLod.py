294da74dd3f6a3aac9971d93452c7b4b9a4a19bc
Stelios Vitalis <steliosvitalis@gmail.com>
2021-08-03 16:50:59 +0200

    Add script to extract lod from a city model

---
+++ extractLod.py
@@ -0,0 +1,40 @@
+import click
+import json
+
+@click.command()
+@click.argument("input", type=click.File("rb"))
+@click.option("-l", "--lod")
+@click.option("-o", "--output", type=click.File("w"))
+def main(input, lod, output):
+    cm = json.load(input)
+
+    if "transform" in cm:
+        s = cm["transform"]["scale"]
+        t = cm["transform"]["translate"]
+        verts = [[v[0] * s[0] + t[0], v[1] * s[1] + t[1], v[2] * s[2] + t[2]]
+                for v in cm["vertices"]]
+    else:
+        verts = cm["vertices"]
+
+    lods = set([geom["lod"] for obj in cm["CityObjects"]
+                            for geom in cm["CityObjects"][obj]["geometry"]])
+
+    if not lod in lods:
+        print("LoD not found in the dataset!")
+        exit()
+
+    for co_id in cm["CityObjects"]:
+        co = cm["CityObjects"][co_id]
+
+        new_geom = []
+
+        for geom in co["geometry"]:
+            if str(geom["lod"]) == str(lod):
+                new_geom.append(geom)
+        
+        co["geometry"] = new_geom
+    
+    json.dump(cm, output)
+
+if __name__ == "__main__":
+    main()
